64c2b587c18a1a165609f84efd252aad3f91c3ca
holydogy <toghrulbakhishov06@gmail.com>
2026-02-25 07:44:44 -0800

    Configure Vite to build all HTML pages

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'admin.html'),
+                login: resolve(__dirname, 'login.html'),
+                register: resolve(__dirname, 'register.html'),
+                postAd: resolve(__dirname, 'post-ad.html'),
+                forgotPassword: resolve(__dirname, 'forgot-password.html'),
+                shops: resolve(__dirname, 'shops.html'),
+                category: resolve(__dirname, 'category.html'),
+                parts: resolve(__dirname, 'parts.html'),
+                accessories: resolve(__dirname, 'accessories.html'),
+                tires: resolve(__dirname, 'tires.html'),
+                oils: resolve(__dirname, 'oils.html'),
+                equipment: resolve(__dirname, 'equipment.html'),
+                cars: resolve(__dirname, 'cars.html'),
+            },
+        },
+    },
+});
